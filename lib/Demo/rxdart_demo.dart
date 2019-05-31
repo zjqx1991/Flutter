f9970f74004a4f759352b0d5cb8ec16f34c4a7d3
RevanWang <zjqx1991@163.com>
2019-05-31 15:03:33 +0800

    subject监听数据

--- lib/Demo/rxdart_demo.dart
+++ lib/Demo/rxdart_demo.dart
@@ -27,15 +27,34 @@ class _RxdartDemoHomeState extends State<_RxdartDemoHome> {
   void initState() {
     super.initState();
 
-    Observable<String> _observable = 
-    // Observable(Stream.fromIterable(['hello', '您好']));
-    Observable.fromFuture(Future.value('Hello~~'));
-    _observable.listen(print);
+    //Observable用法
+    // Observable<String> _observable = 
+    // // Observable(Stream.fromIterable(['hello', '您好']));
+    // Observable.fromFuture(Future.value('Hello~~'));
+    // _observable.listen(print);
+
+    //先监听才可以监听到数据
+    //PublishSubject<String> _subject = PublishSubject<String>();
+
+    BehaviorSubject<String> _subject = BehaviorSubject<String>();
+
+    _subject.listen((data){
+      print('listen 1: ${data}');
+    });
+    //添加数据
+    _subject.add('Hello');
+
+    _subject.listen((data) => print('listen 2:${data.toUpperCase()}'));
+
+    //添加数据
+    _subject.add('World');
+
+    _subject.close();
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
     return Container(
     );
   }
